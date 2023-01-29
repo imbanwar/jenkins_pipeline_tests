fc5b854d44194321f3fdf91d58db3aeb9097a16d
unknown <imbesatanwar90@gmail.com>
2023-01-29 20:40:29 +0530

    added message4

--- tests/test_math_func.py
+++ tests/test_math_func.py
@@ -1,7 +1,12 @@
+import os
 import math_func
 def test_add():
-    assert math_func.add(7,3) == 10
-    assert math_func.add(7) ==9
+    assert math_func.add(value1,value2) == 10
+    assert math_func.add(value1) ==9
 def test_product():
-	assert math_func.product(5,5) ==25
-	assert math_func.product(5) ==10
+	assert math_func.product((value1,value2) ==25
+	assert math_func.product(value1) ==10
+
+value1 = os.getenv("value1")
+value2 = os.getenv("value2")
+

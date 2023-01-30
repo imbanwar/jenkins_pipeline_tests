8131f0f0e876ddc4115b7fb154566ed040584365
unknown <imbesatanwar90@gmail.com>
2023-01-30 08:50:46 +0530

    added fil4

--- src/math_func.py
+++ src/math_func.py
@@ -7,7 +7,7 @@ def product(x, y=2):
     print(x * y)
     print("multiplication completed")
     return x * y
-# input1 = int(os.getenv("value1"))
-# input2 = int(os.getenv("value2"))
-# add(input1, input2)
-# product(input1, input2)
+input1 = int(os.getenv("value1"))
+input2 = int(os.getenv("value2"))
+add(input1, input2)
+product(input1, input2)

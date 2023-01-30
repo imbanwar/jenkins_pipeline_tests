1fddb084311d2db039ce0fc19ad4d3734cb98e9b
unknown <imbesatanwar90@gmail.com>
2023-01-30 07:41:36 +0530

    added files

--- tests/test_math_func.py
+++ tests/test_math_func.py
@@ -1,5 +1,9 @@
 import os
-from src import math_func
+import sys
+sys.path.append(r'C:\Users\Lenovo\OneDrive\Desktop\jenkins_pipeline_tests\src')
+import math_func
+#SET PYTHONPATH=C:\Users\Lenovo\OneDrive\Desktop\jenkins_pipeline_tests\src
+
 def test_add():
     assert math_func.add(value1,value2) == 10
     assert math_func.add(value1) ==9
@@ -8,4 +12,4 @@ def test_product():
 	assert math_func.product(value1) ==10
 value1 = os.getenv("value1")
 value2 = os.getenv("value2")
-
+#print(sys.path)
